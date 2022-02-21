34c2e833805a343646c35f1a20363d6e762be631
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-21 17:36:07 +0700

    Login Using JWT jsonwebtoken

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,9 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); //for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
-exports.signup = (req, res) => {
+exports.signUp = (req, res) => {
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
@@ -14,5 +16,31 @@ exports.signup = (req, res) => {
     res.json({
       user,
     });
+  });
+};
+
+exports.signIn = (req, res) => {
+  //find user base on email
+  const { email, passord } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        err: "User with that email does not exist. Please sign up",
+      });
+    }
+    // if user is found make sure the email and password match
+    //create authenticate method in user model
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password dont match.",
+      });
+    }
+    //generate a  signed token with user id and secret
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    //persist the token as "t" in cookie with expire date
+    res.cookie("t", token, { expire: new Date() + 99999 });
+    // return response with user and token to FE client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
   });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/user");
+const { signUp, signIn } = require("../controllers/user");
 const {userSignupValidator} = require("../validator")
 
-router.post("/signup",userSignupValidator, signup);
+router.post("/signUp",userSignupValidator, signUp);
+router.post("/signIn", signIn);
 
 module.exports = router;
