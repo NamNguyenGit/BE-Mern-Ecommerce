03676682590773863b879bb84b9f8f2d8bbc72ef
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-21 18:25:07 +0700

    Save Profile after login

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+
+const {  requireSignIn } = require("../controllers/auth");
+
+const { userById } = require("../controllers/user");
+
+router.get('/secret/:userId',requireSignIn, (req,res) => {
+    res.json({
+        user: req.profile
+    })
+})
+
+router.param("userId", userById);
+
+module.exports = router;
