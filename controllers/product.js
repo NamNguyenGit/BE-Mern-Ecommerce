ad5bf3c5afb683bb6e5a327bd9a9c4d992973497
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-22 11:50:09 +0700

    Validate Product

--- controllers/product.js
+++ controllers/product.js
@@ -9,26 +9,37 @@ exports.create = (req, res) => {
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
-    
       return res.status(400).json({
         error: "Image could not be uploaded",
       });
     }
-    
-    
+
+    //check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required",
+      });
+    }
+
     let product = new Product(fields);
-  
+
     if (files.photo) {
-     
-      if(files.photo.size>1000000) {
+      if (files.photo.size > 1000000) {
         return res.status(400).json({
-          error: "Image could not be uploaded"
-        })
+          error: "Image should be less than 1mb in size",
+        });
       }
 
-
       product.photo.data = fs.readFileSync(files.photo.filepath);
-     
       product.photo.contentType = files.photo.type;
     }
 
