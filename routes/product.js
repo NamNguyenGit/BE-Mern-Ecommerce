2a60bc2ce077a4778c58a1f1d376358c6cf3dcfd
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-22 12:21:14 +0700

    Update product

--- routes/product.js
+++ routes/product.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove } = require("../controllers/product");
+const { create, productById, read, remove, update } = require("../controllers/product");
 
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
 
 const { userById } = require("../controllers/user");
 
-
-router.post("/product/create/:userId",requireSignIn,isAuth,isAdmin, create);
+router.post("/product/create/:userId", requireSignIn, isAuth, isAdmin, create);
 
 router.get("/product/:productId", read);
-router.delete("/product/:productId/:userId", requireSignIn,isAuth,isAdmin, remove);
+router.delete(
+  "/product/:productId/:userId",
+  requireSignIn,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.put(
+    "/product/:productId/:userId",
+    requireSignIn,
+    isAuth,
+    isAdmin,
+    update
+  );
 
 router.param("userId", userById);
 router.param("productId", productById);
