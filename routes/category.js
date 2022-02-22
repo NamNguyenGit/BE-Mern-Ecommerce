babf91d347d7eebf9e9e7a054c1a1afdab56614f
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-22 14:38:23 +0700

    Update , Dekete , Kist All category

--- routes/category.js
+++ routes/category.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, categoryById, read } = require("../controllers/category");
+const {
+  create,
+  categoryById,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
 
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
 
 const { userById } = require("../controllers/user");
 
 router.get("/category/:categoryId", read);
+router.get("/categories", list);
 
-router.post("/category/create/:userId",requireSignIn,isAuth,isAdmin, create);
+router.post("/category/create/:userId", requireSignIn, isAuth, isAdmin, create);
+router.put(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAuth,
+  isAdmin,
+  update
+);
+
+router.delete(
+  "/category/:categoryId/:userId",
+  requireSignIn,
+  isAuth,
+  isAdmin,
+  remove
+);
 
 router.param("userId", userById);
 router.param("categoryId", categoryById);
