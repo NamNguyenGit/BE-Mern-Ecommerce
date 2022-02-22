2493a73f830d209ec2ea72ef6a431a20d238485e
Nam Nguyen <namnh.bi10-123@st.usth.edu.vn>
2022-02-22 16:26:43 +0700

    User profile read and update

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
 
-const {  requireSignIn, isAdmin, isAuth } = require("../controllers/auth");
+const { userById, read, update } = require("../controllers/user");
 
-const { userById } = require("../controllers/user");
+router.get("/secret/:userId", requireSignIn, isAuth, isAdmin, (req, res) => {
+  res.json({
+    user: req.profile,
+  });
+});
 
-router.get('/secret/:userId',requireSignIn, isAuth, isAdmin, (req,res) => {
-    res.json({
-        user: req.profile
-    })
-})
+router.get('/user/:userId',requireSignIn, isAuth, read )
+
+router.put('/user/:userId',requireSignIn, isAuth, update )
+
 
 router.param("userId", userById);
 
